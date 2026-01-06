bf913eaf3401ede6e751a09d0f03768282e789bd
idomaori98 <idoma@mta.ac.il>
2026-01-06 21:22:00 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+    { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+export default function App() {
+    return (
+        <div className="app">
+            <Logo></Logo>
+            <Form></Form>
+            <PackingList></PackingList>
+            <Stats></Stats>
+        </div>
+    );
+}
+
+function Logo() {
+    return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+    return (
+        <div className="add-form">
+            <h3>What do you need for your 😍 trip ?</h3>
+        </div>
+    );
+}
+
+function PackingList() {
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+                {item.description} {item.quantity}
+            </span>
+            <button>❌</button>
+        </li>
+    );
+}
+
+function Stats() {
+    return (
+        <footer className="stats">
+            <em>
+                🧳 You have X items on your list, and you already packed X (X%)
+            </em>
+        </footer>
+    );
+}
